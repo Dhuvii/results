20d05a0a401f3a5ac8017926bfc3a125b29010dd
Dhuvii <thuvaraganuideisgns@gmail.com>
2023-08-30 21:55:19 +0530

    marks added.

--- src/store/resultStore.ts
+++ src/store/resultStore.ts
@@ -4,7 +4,7 @@ type Result = {
   index: string;
   name: string;
   grade: string;
-  rank: number;
+  rank: string;
   subjects: {
     name: string;
     grade: string;
@@ -21,11 +21,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230201",
       name: "M.Nilukshan",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "AB",
         },
       ],
     },
@@ -33,11 +33,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230202",
       name: "J.Hariprashan",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -45,11 +45,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230203",
       name: "N.Thivagaran",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -58,11 +58,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230204",
       name: "J.Johnkishan",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -70,11 +70,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230205",
       name: "T.Manoj",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -82,11 +82,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230206",
       name: "T.Arun",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -95,11 +95,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230207",
       name: "M.Harigaran",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -108,11 +108,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230208",
       name: "S.Jagathishvaran",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -121,11 +121,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230209",
       name: "S.Thanushan",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -133,11 +133,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230210",
       name: "C.Kirushoth",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -146,11 +146,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230211",
       name: "R.Navanithan",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -159,11 +159,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230212",
       name: "A.lakshika",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -172,11 +172,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230213",
       name: "P.Nilushini",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "AB",
         },
       ],
     },
@@ -185,11 +185,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230214",
       name: "S.Sashina",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "AB",
         },
       ],
     },
@@ -198,11 +198,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230215",
       name: "Y.Uthayashini",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "AB",
         },
       ],
     },
@@ -211,11 +211,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230216",
       name: "P.Nishaptha",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -224,11 +224,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230217",
       name: "S.Shalini",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "AB",
         },
       ],
     },
@@ -237,11 +237,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230218",
       name: "K.Roshika",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "AB",
         },
       ],
     },
@@ -250,11 +250,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230219",
       name: "Y.Piriyavathani",
       grade: "10",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "AB",
         },
       ],
     },
@@ -263,11 +263,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230220",
       name: "V.Akshan",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "AB",
         },
       ],
     },
@@ -276,11 +276,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230221",
       name: "K.Diroshan",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -289,11 +289,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230222",
       name: "R.Pavithiran",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -302,11 +302,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230223",
       name: "K.Sanjevankanth",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -315,11 +315,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230224",
       name: "V.Moharish",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -328,11 +328,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230225",
       name: "P.Thivankumar",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -341,11 +341,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230226",
       name: "M.Thanushkar",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -354,11 +354,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230227",
       name: "K.Arun",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -367,11 +367,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230228",
       name: "S.Thanush",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -380,11 +380,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230229",
       name: "S.Sriraj",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "AB",
         },
       ],
     },
@@ -393,11 +393,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230230",
       name: "V.Kabishek",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -406,11 +406,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230231",
       name: "V.Thanojan",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "AB",
         },
       ],
     },
@@ -419,11 +419,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230232",
       name: "M.Haricharan",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -432,11 +432,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230233",
       name: "V.Thilukshika",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -445,11 +445,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230234",
       name: "R.Thilrukshi",
       grade: "11",
-      rank: 0,
+      rank: "2",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "s",
         },
       ],
     },
@@ -458,11 +458,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230235",
       name: "J.Piravina",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -471,11 +471,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230236",
       name: "S.Rishanthini",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "AB",
         },
       ],
     },
@@ -484,11 +484,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230237",
       name: "C.Kirishanthika",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -497,11 +497,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230238",
       name: "M.Nathushika",
       grade: "11",
-      rank: 0,
+      rank: "1",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "c",
         },
       ],
     },
@@ -510,11 +510,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230239",
       name: "B.Yugatharshini",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -523,11 +523,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230240",
       name: "E.Manoransani",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -536,11 +536,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230241",
       name: "K.Abisha",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -549,11 +549,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230242",
       name: "S.Sathurshika",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -562,11 +562,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230243",
       name: "S.Thanushiya",
       grade: "11",
-      rank: 0,
+      rank: "3",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "s",
         },
       ],
     },
@@ -575,11 +575,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230244",
       name: "K.Mathushiya",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
@@ -588,11 +588,11 @@ const useResultStore = create<ResultState>()(() => ({
       index: "20230245",
       name: "S.Kobika",
       grade: "11",
-      rank: 0,
+      rank: "-",
       subjects: [
         {
           name: "Mathematics",
-          grade: "",
+          grade: "w",
         },
       ],
     },
